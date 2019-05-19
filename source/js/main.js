e479a2f8320d027bf09d5aacc36e920d123b58b6
Uporold <crubick1337@gmail.com>
2019-05-19 13:59:07 +0300

    Адаптивные декоративные элементы

--- source/js/main.js
+++ source/js/main.js
@@ -1,53 +1,54 @@
- var navMain = document.querySelector('.main-nav');
- var navToggle = document.querySelector('.page-header__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('main-nav--no-js');
 
 
+navToggle.addEventListener('click', function () {
+	if (navMain.classList.contains('main-nav--closed')) {
+		navMain.classList.remove('main-nav--closed');
+		navMain.classList.add('main-nav--opened');
+	} else {
+		navMain.classList.add('main-nav--closed');
+		navMain.classList.remove('main-nav--opened');
+	}
+});
 
- navToggle.addEventListener('click', function () {
-     if (navMain.classList.contains('main-nav--closed')) {
-         navMain.classList.remove('main-nav--closed');
-         navMain.classList.add('main-nav--opened');
-     } else {
-         navMain.classList.add('main-nav--closed');
-         navMain.classList.remove('main-nav--opened');
-     }
- });
 
+/* Карта */
 
- /* Карта */
+ymaps.ready(function () {
+	var myMap = new ymaps.Map('map', {
+			center: [59.93913800574294, 30.321521561386636],
+			zoom: 17
+		}, {
+			searchControlProvider: 'yandex#search'
+		}),
 
- ymaps.ready(function () {
-     var myMap = new ymaps.Map('map', {
-             center: [59.93913800574294, 30.321521561386636],
-             zoom: 17
-         }, {
-             searchControlProvider: 'yandex#search'
-         }),
+		// Создаём макет содержимого.
+		MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+			'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+		),
 
-         // Создаём макет содержимого.
-         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-         ),
+		myPlacemarkWithContent = new ymaps.Placemark([59.93874546240775, 30.323177881614683], {
 
-         myPlacemarkWithContent = new ymaps.Placemark([59.93874546240775, 30.323177881614683], {
+		}, {
+			// Опции.
+			// Необходимо указать данный тип макета.
+			iconLayout: 'default#imageWithContent',
+			// Своё изображение иконки метки.
+			iconImageHref: '../img/map-marker.png',
+			// Размеры метки.
+			iconImageSize: [231, 190],
+			// Смещение левого верхнего угла иконки относительно
+			// её "ножки" (точки привязки).
+			iconImageOffset: [-54, -193],
+			// Смещение слоя с содержимым относительно слоя с картинкой.
 
-         }, {
-             // Опции.
-             // Необходимо указать данный тип макета.
-             iconLayout: 'default#imageWithContent',
-             // Своё изображение иконки метки.
-             iconImageHref: '../img/map-marker.png',
-             // Размеры метки.
-             iconImageSize: [231, 190],
-             // Смещение левого верхнего угла иконки относительно
-             // её "ножки" (точки привязки).
-             iconImageOffset: [-54, -193],
-             // Смещение слоя с содержимым относительно слоя с картинкой.
+			// Макет содержимого.
+			iconContentLayout: MyIconContentLayout
+		});
 
-             // Макет содержимого.
-             iconContentLayout: MyIconContentLayout
-         });
-
-     myMap.geoObjects
-         .add(myPlacemarkWithContent);
- });
+	myMap.geoObjects
+		.add(myPlacemarkWithContent);
+});
