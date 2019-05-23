38b49652b239f38fe005e35550f772bfa3d94a72
Uporold <crubick1337@gmail.com>
2019-05-23 16:42:45 +0300

    Векторная графика и оптимизация

--- source/js/main.js
+++ source/js/main.js
@@ -5,50 +5,56 @@ navMain.classList.remove('main-nav--no-js');
 
 
 navToggle.addEventListener('click', function () {
-	if (navMain.classList.contains('main-nav--closed')) {
-		navMain.classList.remove('main-nav--closed');
-		navMain.classList.add('main-nav--opened');
-	} else {
-		navMain.classList.add('main-nav--closed');
-		navMain.classList.remove('main-nav--opened');
-	}
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
 });
 
 
 /* Карта */
 
 ymaps.ready(function () {
-	var myMap = new ymaps.Map('map', {
-			center: [59.93905635443461,30.319621272460942],
-			zoom: 17
-		}, {
-			searchControlProvider: 'yandex#search'
-		}),
+  var myMap = new ymaps.Map('map', {
+      center: [59.93905635443461,30.319621272460942],
+      zoom: 17
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-		// Создаём макет содержимого.
-		MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-			'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-		),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
 
-		myPlacemarkWithContent = new ymaps.Placemark([59.938625680716555,30.323033042327896], {
+    myPlacemarkWithContent = new ymaps.Placemark([59.938625680716555,30.323033042327896], {
 
-		}, {
-			// Опции.
-			// Необходимо указать данный тип макета.
-			iconLayout: 'default#imageWithContent',
-			// Своё изображение иконки метки.
-			iconImageHref: '../img/map-pin.png',
-			// Размеры метки.
-			iconImageSize: [124, 106],
-			// Смещение левого верхнего угла иконки относительно
-			// её "ножки" (точки привязки).
-			iconImageOffset: [-50, -130],
-			// Смещение слоя с содержимым относительно слоя с картинкой.
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: '../img/map-pin.png',
+      // Размеры метки.
+      iconImageSize: [124, 106],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-50, -130],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
 
-			// Макет содержимого.
-			iconContentLayout: MyIconContentLayout
-		});
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
+    });
 
-	myMap.geoObjects
-		.add(myPlacemarkWithContent);
+  myMap.geoObjects
+    .add(myPlacemarkWithContent);
+
+
 });
+
+  if (window.innerWidth < 768){
+  myMap.center = [158.93863106417265,30.3230545];
+}
